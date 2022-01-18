be2e5e70a5a76c8b01b975f3cf7013b6e4dcf604
Christopher <christopher.black.2209@googlemail.com>
2022-01-18 00:29:49 +0000

    commit tree 96fbfa9714ad9abddb91743be8351185f878f26c

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => {
+          return (
+            <div key={index}>
+              <h3>Name: {movie.title}</h3>
+              Time: {movie.time}<br />
+              Genres:
+              <ul>
+                {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)}
+              </ul>
+            </div>
+          )
+        })}
     </div>
   );
 };
